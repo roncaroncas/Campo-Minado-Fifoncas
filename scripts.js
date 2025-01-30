8d33b84c6b41eb842114a46bb73a34bd996fb68e
Roncas <victor.roncalli.souza@gmail.com>
2025-01-30 19:55:33 -0300

    transforma em X quando clica

--- scripts.js
+++ scripts.js
@@ -1,7 +1,47 @@
-function trocarNXO(){
-	window.alert("oi")
+function trocarNXO(id){
+	document.getElementById(id).textContent="X";
 }
 
 function newGame(){
-	window.alert("tchau")
+
+	/*
+
+	//Removendo toda a tabela de id = board
+
+	var new_tbody = document.createElement('tbody');
+	populate_with_new_rows(new_tbody);
+	old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
+
+	var tbl = document.getElementById("board");
+    if(tbl){
+    	tbl.parentNode.removeChild(tbl);
+    }
+
+    window.alert("tchau")
+   	}
+    
+
+	var body = document.getElementsByTagName('body')[0];
+	var tbl = document.createElement('table');
+	tbl.style.width = '100%';
+	tbl.setAttribute('border', '1');
+	var tbdy = document.createElement('tbody');
+	for (var i = 0; i < 3; i++) {
+    	var tr = document.createElement('tr');
+    	for (var j = 0; j < 2; j++) {
+      		if (i == 2 && j == 1) {
+        		break
+      		} else {
+        		var td = document.createElement('td');
+        		td.appendChild(document.createTextNode('\u0020'))
+		        i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+		        tr.appendChild(td)
+      		}
+    	}
+    tbdy.appendChild(tr);
+  }
+  tbl.appendChild(tbdy);
+  body.appendChild(tbl)
 }
+
+*/
